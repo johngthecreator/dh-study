54d2e4b49cff8c9090b7a6cc819e50837f30547f
John Gorriceta <johngorriceta@Johns-MacBook-Air.local>
2023-10-21 03:05:51 -0600

    changed up menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,49 @@
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    IconButton
-  } from '@chakra-ui/react'
+// import {
+//     Menu,
+//     MenuButton,
+//     MenuList,
+//     MenuItem,
+//     IconButton
+//   } from '@chakra-ui/react'
+import { Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, useDisclosure, IconButton } from '@chakra-ui/react'
 import { HamburgerIcon } from "@chakra-ui/icons";
 import {useNavigate} from 'react-router-dom';
+import { useRef } from 'react';
 
 
 export default function Header(){
     const navigate = useNavigate();
+    const btnRef = useRef();
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return(
         <header className="flex flex-row bg-[#3E69A3] p-5 items-center">
-            <Menu>
-                <MenuButton
-                    as={IconButton}
-                    aria-label='Options'
-                    icon={<HamburgerIcon boxSize={8}/>}
-                    variant='none'
-                    className='text-white font-bold'
-                />
-                <MenuList>
-                    <MenuItem onClick={()=>navigate("/study")}>
-                        Study
-                    </MenuItem>
-                </MenuList>
-            </Menu>
+            <div className='flex flex-row gap-5 items-center'>
+            <IconButton
+            colorScheme="dark"
+            aria-label='menu button'
+            icon={<HamburgerIcon boxSize={8}/>}
+            ref={btnRef}
+            onClick={onOpen}
+            />
+            </div>
+            <Drawer
+            isOpen={isOpen}
+            placement='left'
+            onClose={onClose}
+            finalFocusRef={btnRef}
+            >
+            <DrawerOverlay />
+            <DrawerContent
+            maxW="200px"
+            backgroundColor="white">
+                <DrawerBody>
+                <div className="grid grid-columns-1 w-full">
+                    <button className="text-center font-extrabold text-[#3E69A3] duration-200 text-xl px-5 py-2" onClick={()=>{navigate("/")}} >Home</button>
+                    <button className="text-center font-extrabold text-[#3E69A3] duration-200 text-xl px-5 py-2" onClick={()=>{navigate("/study")}} >Study</button>
+                </div>
+                </DrawerBody>
+            </DrawerContent>
+            </Drawer>
             <a href='/'>
                 <h1 className='text-3xl text-white font-extrabold mx-3'>PureLearn</h1>
             </a>
