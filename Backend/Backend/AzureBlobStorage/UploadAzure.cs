079bd056b8fcec1ad993fe2c0e9adea45b3c4067
Sebastian Dunn <sebastiancdunn@byui.edu>
2023-10-20 23:21:11 -0600

    Upload file and metadata

--- Backend/Backend/AzureBlobStorage/UploadAzure.cs
+++ Backend/Backend/AzureBlobStorage/UploadAzure.cs
@@ -1,5 +1,9 @@
+using Azure;
 using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Newtonsoft.Json;
 
 namespace Backend.AzureBlobStorage;
 
@@ -20,13 +24,87 @@ public class UploadAzure
         return _configuration.GetConnectionString(sensitiveSettingsKey);
     }
 
-    public async void UploadToBlob(string userId, string sessionId, string filePath)
+    public async Task UploadToCloud(string userId, string sessionId, string localFilePath, string actualFileName)
     {
-        await _blobUserContainer.CreateIfNotExistsAsync();
-        // The following also creates a blob container if it doesn't already exist
-        string blobFilePath = $"{userId}/{sessionId}/{filePath}";
+        // The following creates a blob item if it doesn't already exist
+        string blobFilePath = $"{userId}/{sessionId}/Content/{actualFileName}";
         BlobClient blobClient = _blobUserContainer.GetBlobClient(blobFilePath);
-        await using FileStream fs = File.OpenRead(blobFilePath);
-        await blobClient.UploadAsync(fs, true);
+
+        if (await UploadFileContent(blobClient, localFilePath, userId, sessionId))
+        {
+            await AddFileToMetadata(userId, sessionId, blobClient, actualFileName);
+        }
+    }
+
+    private static Dictionary<string, string> SetTags(string userId, string sessionId)
+    {
+        return new Dictionary<string, string>
+            {
+                { "userId", userId },
+                { "sessionId", sessionId },
+                { "uploadDate", DateTime.Now.ToShortDateString() }
+            };
+    }
+
+    private async Task AddFileToMetadata(string userId, string sessionId, BlobClient fileBlob, string fileName)
+    {
+        try
+        {
+            await UpdateSessionMetaData(sessionId, userId, fileBlob, fileName);
+        }
+        catch (RequestFailedException e)
+        {
+            Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+            Console.WriteLine(e.Message);
+            Console.ReadLine();
+        }
+    }
+
+    private static async Task<bool> UploadFileContent(BlobClient blobClient, string localFilePath, string userId, string sessionId)
+    {
+        try
+        {
+            await using FileStream fs = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(fs, true);
+            await blobClient.SetTagsAsync(SetTags(userId, sessionId));
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+        
+    }
+
+    private async Task UpdateSessionMetaData(string sessionId, string userId, BlobBaseClient fileBlob, string fileName)
+    {
+        BlobClient? metadataBlobClient = _blobUserContainer.GetBlobClient($"{userId}/{sessionId}/metadata.json");
+        SessionMetadata sessionMetadata = new SessionMetadata();
+
+        try
+        {
+            Response<BlobDownloadInfo>? response = await metadataBlobClient.DownloadAsync();
+            using StreamReader reader = new StreamReader(response.Value.Content);
+            string existingMetadataJson = await reader.ReadToEndAsync();
+            sessionMetadata = JsonConvert.DeserializeObject<SessionMetadata>(existingMetadataJson);
+        }
+        catch (Exception e)
+        {
+            //TODO: Add throw can't find metadata file
+            Console.WriteLine(e);
+            metadataBlobClient = _blobUserContainer.GetBlobClient($"{userId}/{sessionId}/metadata.json");
+        }
+        
+
+        // Get blob info and update metadata
+        Response<GetBlobTagResult>? tags = await fileBlob.GetTagsAsync();
+        sessionMetadata.Files[fileName] = tags.Value.Tags["uploadDate"];
+
+            // Serialize updated metadata to JSON
+        string updatedMetadataJson = JsonConvert.SerializeObject(sessionMetadata, Formatting.Indented);
+
+        // Upload updated metadata to blob
+        using MemoryStream stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(updatedMetadataJson));
+        await metadataBlobClient.UploadAsync(stream, overwrite: true);
     }
 }
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Authentication;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("sensitivesettings.json", optional: true);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,8 +29,6 @@ FirebaseApp.Create(new AppOptions
 {
     Credential = GoogleCredential.FromFile("firebasekey.json")
 });
-
-builder.Configuration.AddJsonFile("sensitivesettings.json", optional: true);
 
 var app = builder.Build();
 
