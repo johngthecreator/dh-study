8737f9f77ef132929773e450f928f0ec47306cb5
Shelby Vasas <shelbyvasas@gmail.com>
2023-10-20 20:39:34 -0600

    Upload file functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Upload from './pages/Upload';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   )
